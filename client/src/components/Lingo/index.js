d2f951d281ae722b67ef0785f502464c597bd0db
Brendan Morse <42715623+bmmorse@users.noreply.github.com>
2023-03-01 01:05:56 -0500

    update

--- client/src/components/Lingo/index.js
+++ client/src/components/Lingo/index.js
@@ -4,13 +4,36 @@ import styled from 'styled-components/macro';
 // does this work
 
 const DIV_FULL = styled.div`
-  display: flex;
-  width: 100%;
-  flex-wrap: wrap;
-  padding: 16px;
-  gap: 16px;
-  img {
-    width: calc((100% - (4 * 16px)) / 5);
+  .header {
+    width: 100%;
+    background: #f6f6f6;
+    border-bottom: solid 1px #eaeaea;
+    padding: 40px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    a {
+      color: #1a0dab;
+    }
+  }
+
+  .container {
+    display: flex;
+    width: 100%;
+    flex-wrap: wrap;
+    padding: 16px;
+    gap: 16px;
+    img {
+      width: calc((100% - (4 * 16px)) / 5);
+    }
+
+    .empty {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 80px 0 0 0;
+    }
   }
 `;
 
@@ -31,7 +54,11 @@ export default function Lingo() {
 
   function displayImages() {
     if (data.length === 0) {
-      return <h1>There are no images in the Kit</h1>;
+      return (
+        <div className='empty'>
+          <h3>There are no images in the Kit</h3>
+        </div>
+      );
     }
     return data.map((img) => {
       return <img key={img} src={img}></img>;
@@ -48,5 +75,14 @@ export default function Lingo() {
     getData();
   }, []);
 
-  return <DIV_FULL>{displayImages()}</DIV_FULL>;
+  return (
+    <DIV_FULL>
+      <div className='header'>
+        <a href='https://www.lingoapp.com/109576/s/PgqAx2/?v=0'>
+          Link to Lingo Kit
+        </a>
+      </div>
+      <div className='container'>{displayImages()}</div>
+    </DIV_FULL>
+  );
 }
